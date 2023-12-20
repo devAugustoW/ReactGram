0205cac210db7d9c7b8a4fcb2a1efaa7b69c7360
Augusto Dantas <augustodantas.dev@gmail.com>
2023-12-19 21:18:36 -0300

    setup backend, rotas e conexao com bd

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,29 @@
+require("dotenv").config(); 
+const port = process.env.PORT;
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-
-const port = 5000;
 
 const app = express();
 
 // config JSON and form data response
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
+
+// resolver problemas de CORS
+app.use(cors({credentials: true, origin: "http://localhost:3000" }))
+
+// Diretório de upload
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+// Conexão com Banco de Dados
+require("./config/db.js")
+
+// routes
+const router = require("./routes/Router");
+app.use(router);
+
 
 app.listen(port, () => {
    console.log(`App rodando na porta ${port}`);
