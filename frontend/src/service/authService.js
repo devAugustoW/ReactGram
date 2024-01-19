752e4680a6b8d864f9f60de691a26f89815a0264
Augusto Dantas <augustodantas.dev@gmail.com>
2024-01-19 20:15:54 -0300

    Update

--- frontend/src/service/authService.js
+++ frontend/src/service/authService.js
@@ -6,7 +6,7 @@ const register = async(data) => {
    const config = requestConfig("POST", data);
 
    try {
-      const res = await fetch(api + "users/register", config)
+      const res = await fetch(api + "Users/register", config)
          .then((res) => res.json())
          .catch((err) => err)
          
